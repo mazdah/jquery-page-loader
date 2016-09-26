e95ae9b59a551e9fde9c130f8896142494d73bbe
paikwiki <paikwiki@gmail.com>
2016-09-26 10:18:52 +0900

    콜백 함수로 div.loading 엘리먼트 제거

--- js/script.js
+++ js/script.js
@@ -1,11 +1,7 @@
-jQuery(document).ready(function($){
-	console.log('js works');
-});
-
 $(window).load(function() {
- $('.loading').animate({
-   opacity: 0
- }, 1000).hide(0, function() {
-   console.log('cover hided');
- });
+  $('.loading').animate({
+		opacity: 0
+	}, 600,function() {
+		$(this).remove();
+	});
 });
